a6d04d0195ffdb869b34d8b0a56f7735198e36ec
chelseaerinavery <chelsea.erin.avery@gmail.com>
2023-09-04 21:43:47 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const connectionString = `mongodb+srv://cavery:newslang@cluster0.cmtay5q.mongodb.net/?retryWrites=true&w=majority`
+
+
+
+
+MongoClient.connect(connectionString)
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('star-wars-quotes')
+        const quotesCollection = db.collection('quotes')
+
+        //middlewares
+        app.set('view engine', 'ejs')
+        app.use(express.static('public'))
+        app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(bodyParser.json())
+
+        //routes
+        app.get('/', (req, res) => {
+            db.collection('quotes')
+                .find()
+                .toArray()
+                .then(results => {
+                    res.render('index.ejs', { quotes: results })
+                })
+                .catch(/* ... */)
+        })
+        app.post('/quotes', (req, res) => {
+            quotesCollection
+                .insertOne(req.body)
+                .then(result => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+        app.put('/quotes', (req, res) => {
+            quotesCollection
+                .findOneAndUpdate(
+                    { name: 'Darth Vader' },
+                    {
+                        $set: {
+                            name: req.body.name,
+                            quote: req.body.quote,
+                        },
+                    },
+                    {
+                        upsert: true,
+                    }
+                )
+                .then(result => {
+                    res.json('Success')
+                })
+                .catch(error => console.error(error))
+        })
+        //app.delete
+
+        //listen
+        app.listen(3000, function () { console.log('listening on 3000') })
+    })
+    .catch(error => console.error(error))
+
+// app.get('/', function (req, res) { res.send('Hello World') })
+
+// app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html') })
+
+// app.post('/quotes', (req, res) => {
+//     console.log(req.body)
+// })
